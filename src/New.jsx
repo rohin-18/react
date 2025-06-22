a1d02b2bc2ddcdf97f6e71ab388f3a1f751454f8
rohin <rohinprasanthr.23eie@kongu.edu>
2025-06-22 20:59:14 +0530

    Day-6 Commit

---
+++ src/New.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+function New(parameter) {
+    let [count, setCount] = useState(0)
+    return (
+        <div>
+            <h1>{parameter.name} home</h1>
+            <button onClick={() => {
+                setCount(count + 1);
+            }}>Click</button>
+            <p>{count}</p>
+        </div >
+    )
+}
+export default New;
+
